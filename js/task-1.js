7547aee019d368b69f3c339ce39e6dcef0d68feb
AfAlina190 <130515756+AfAlina190@users.noreply.github.com>
2024-01-14 23:23:30 +0100

    work on mistakes

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,19 @@
-function makeTransaction(quantity, priceProDroid) {
-  let totalPreice = quantity * priceProDroid;
+// function makeTransaction(quantity, priceProDroid) {
+//   let totalPreice = quantity * priceProDroid;
 
-  return "You ordered " + quantity + " droids worth " + totalPreice +" credits!";
+//   return "You ordered " + quantity + " droids worth " + totalPreice +" credits!";
+// }
+
+// console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+// console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+// console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+function makeTransaction(quantity, pricePerDroid) {
+
+    const totalPrice = quantity * pricePerDroid;
+
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
-
-
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
-
--- js/task-3.js
+++ js/task-3.js
@@ -12,3 +12,35 @@ return totalWidth;
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+// ´´´´´´´´´´´´´´´´´´´´´´
+// function getElementWidth(content, padding, border) 
+// {
+
+// let content = parseFloat(content);
+// let padding = parseFloat(padding);
+// let border = parseFloat(border);
+    
+//     return totalWidth = content + padding + (border * 4);
+// }
+
+// console.log(getElementWidth("50px", "8px", "4px")); // 74
+// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+// console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+
+// ´´´´´´´´´´´´´´´´´´´´´´
+// function getElementWidth(content, padding, border) {
+
+// const contentWidth = parseFloat(content);
+// const paddingWidth = parseFloat(padding);
+// const borderWidth = parseFloat(border);
+
+//   const totalWidth = contentWidth + paddingWidth + (borderWidth * 4);
+
+// return totalWidth;
+// }
+
+// console.log(getElementWidth("50px", "8px", "4px")); // 74
+// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+// console.log(getElementWidth("200px", "0px", "0px")); // 200
